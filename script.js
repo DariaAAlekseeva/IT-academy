396b218ba1274cfd791b5424eea512cadb8e1b40
DariaAAlekseeva <darjaasm@gmail.com>
2023-10-30 12:20:52 +0100

    сode is formatted

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function multyply(){
-    let [num1,num2]=getNumbersValue(); 
-    document.getElementById('result').innerHTML= num1*num2;
-   }
-   
-   function divide(){
-    let [num1, num2]=getNumbersValue();
-    document.getElementById('result').innerHTML= num1/num2;
-   }
-   
-   function plus(){
-    let [num1,num2]=getNumbersValue();
-    document.getElementById('result').innerHTML= num1+num2;
-   }
-   
-   function minus(){
-    let [num1,num2]=getNumbersValue();
-    document.getElementById('result').innerHTML= num1-num2;
-   }
-   
-   function getNumbersValue(){
-     arr=[];
-     let a = document.getElementById('number1').value;
-     let b = document.getElementById('number2').value;
-     arr.push(Number(a));
-     arr.push(Number(b));
-     return arr;
-   }
+function multyply() {
+    let [num1, num2] = getNumbersValue();
+    document.getElementById('result').innerHTML = num1 * num2;
+}
+
+function divide() {
+    let [num1, num2] = getNumbersValue();
+    document.getElementById('result').innerHTML = num1 / num2;
+}
+
+function plus() {
+    let [num1, num2] = getNumbersValue();
+    document.getElementById('result').innerHTML = num1 + num2;
+}
+
+function minus() {
+    let [num1, num2] = getNumbersValue();
+    document.getElementById('result').innerHTML = num1 - num2;
+}
+
+function getNumbersValue() {
+    arr = [];
+    let a = document.getElementById('number1').value;
+    let b = document.getElementById('number2').value;
+    arr.push(Number(a));
+    arr.push(Number(b));
+    return arr;
+}
